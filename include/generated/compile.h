b022e3911d4b9fb7943ca6f204b3d1c74938924a
xdatravelbug <ch.butz@gmail.com>
2015-12-14 13:11:53 +0100

    First commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 SMP PREEMPT Mon Nov 30 10:57:38 GMT 2015"
+#define LINUX_COMPILE_BY "osboxes"
+#define LINUX_COMPILE_HOST "osboxes"
+#define LINUX_COMPILER "gcc version 4.7.3 20130328 (prerelease) (crosstool-NG linaro-1.13.1-4.7-2013.04-20130415 - Linaro GCC 2013.04) "
