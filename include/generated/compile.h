3e74193c6d24d6558402099f48bc36f0a87f3d4c
xdatravelbug <ch.butz@gmail.com>
2015-12-14 21:01:33 +0100

    Added SmartassV2 scheduler and simple IO

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 3 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 SMP PREEMPT Mon Nov 30 10:57:38 GMT 2015"
+#define UTS_VERSION "#3 SMP PREEMPT Mon Dec 14 17:35:29 CET 2015"
 #define LINUX_COMPILE_BY "osboxes"
 #define LINUX_COMPILE_HOST "osboxes"
 #define LINUX_COMPILER "gcc version 4.7.3 20130328 (prerelease) (crosstool-NG linaro-1.13.1-4.7-2013.04-20130415 - Linaro GCC 2013.04) "
